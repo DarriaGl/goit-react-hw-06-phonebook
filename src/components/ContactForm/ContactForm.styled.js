b76401991ba14ef54bdb052dda73a85e06b48d47
DarriaGl <gladchenkodasha13@gmail.com>
2023-09-04 16:09:14 +0300

    update

---
+++ src/components/ContactForm/ContactForm.styled.js
@@ -0,0 +1,28 @@
+import styled from '@emotion/styled';
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+`;
+
+export const Input = styled.input`
+  width: 350px;
+  padding: 10px;
+  border-radius: 5px;
+  margin-top: 5px;
+  margin-bottom: 5px;
+  border-color: olive;
+`;
+
+export const Button = styled.button`
+  padding: 10px 20px;
+  margin-top: 15px;
+  font-size: 14px;
+  border-radius: 6px;
+  border: 1px solid;
+`;
+
+export const Text = styled.p`
+  margin-bottom: 5px;
+`;
---
+++ src/components/Filter/Filter.styled.js
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const Input = styled.input`
+  width: 350px;
+  padding: 10px;
+  border-radius: 5px;
+  border-color: olive;
+`;
